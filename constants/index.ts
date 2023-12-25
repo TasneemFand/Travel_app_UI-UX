32b7e03676f2c11111f60ccd0fa8ee03428c2980
Tasneem Fandakli <tasnimtepe@gmail.com>
2023-12-25 18:38:08 +0300

    create camp section

--- constants/index.ts
+++ constants/index.ts
@@ -42,3 +42,10 @@ export const NAV_LINKS = [
       '/wordpress.svg',
     ],
   };
+
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
